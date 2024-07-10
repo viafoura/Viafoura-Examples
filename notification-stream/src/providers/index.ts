a87baabeb1f638c1ab2906492e0a8ada0ed9eb8f
Martin De Simone <mdesimone@itcrowdarg.com>
2024-07-10 13:35:12 -0300

    Pass VF user id to method

--- notification-stream/src/providers/index.ts
+++ notification-stream/src/providers/index.ts
@@ -6,7 +6,11 @@ import { createPushlyNotification } from "./pushly";
 
 export const createNotification = ((notification: INotification) => {
     const formattedNotification = notificationToFormattedString(notification);
+    let recipientUserId;
+    if(notification.recipients.length > 0 && notification.recipients[0].viafoura_id){
+        recipientUserId = String(notification.recipients[0].viafoura_id.id);
+    }
 
-    createOneSignalNotification(formattedNotification);
-    createPushlyNotification(formattedNotification);
+    createOneSignalNotification(formattedNotification, recipientUserId);
+    createPushlyNotification(formattedNotification, recipientUserId);
 });
