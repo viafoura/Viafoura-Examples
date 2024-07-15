2f5c50a7d05562a3e53d3b99d338f1a8f0b3eb0a
Martin De Simone <mdesimone@itcrowdarg.com>
2024-07-15 12:50:37 -0300

    Error handling

--- notification-stream/src/index.ts
+++ notification-stream/src/index.ts
@@ -1,6 +1,8 @@
 import router from "./routes";
 import path from 'path';
 
+import { Request, Response, NextFunction } from 'express'
+
 require('dotenv').config({path: path.join(__dirname, '..', '.env')})
 
 const express = require('express')
@@ -9,6 +11,10 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use("/", router);
+
+app.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
+  return res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
